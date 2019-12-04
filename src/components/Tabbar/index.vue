fff876510eec3406165fe83a943ba272a36a2c3e
zbzbzzz <1849633183@qq.com>
2019-12-04 11:50:41 +0800

    初始化项目

---
+++ src/components/Tabbar/index.vue
@@ -0,0 +1,80 @@
+<template>
+	<van-tabbar v-model="active" style="z-index: 1999">
+		<van-tabbar-item
+			v-for="(tab, index) in tabbar"
+			:icon="tab.icon"
+			:to="tab.path"
+			:dot="tab.dot"
+			:info="tab.info"
+			:key="index">
+			{{tab.name}}
+		</van-tabbar-item>
+	</van-tabbar>
+</template>
+
+
+<script>
+import { Tabbar, TabbarItem } from 'vant';
+
+export default {
+  data() {
+    return {
+      active: 0,
+      tabbar: [
+        {
+          name: '外卖',
+          path: '/',
+          pathName: 'home',
+          icon: 'eliaomo',
+          dot: false,
+          info: ''
+        },     
+        {
+          name: '购物车',
+          path: '/order',
+          pathName: 'cart',
+          icon: 'cart',
+          dot: false,
+          info: ''
+        },
+        {
+          name: '我的',
+          path: '/user',
+          pathName: 'user',
+          icon: 'wode',
+          dot: false,
+          info: ''
+        }
+      ]
+    };
+  },
+
+  watch: {
+    $route: 'changeActive'
+  },
+
+  created() {
+    const toName = this.$route.name;
+    this.setActive(toName);
+  },
+
+  methods: {
+    changeActive({ name }) {
+      this.setActive(name);
+    },
+    setActive(name) {
+      this.tabbar.forEach((tab, i) => {
+        if (tab.pathName == name) {
+          this.active = i;
+          return false;
+        }
+      });
+    }
+  },
+
+  components: {
+    [Tabbar.name]: Tabbar,
+    [TabbarItem.name]: TabbarItem
+  }
+};
+</script>
